058487a04e4c46fe1e9f89e1315e39fd5ff99eae
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-06-27 10:20:59 -0400

    Added riddle, working on tools

--- src/components/GranRightPath.js
+++ src/components/GranRightPath.js
@@ -7,6 +7,11 @@ function GranRightPath() {
         history.push(path)
     }
     
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e.target)
+    }
+
     return (
         <div className="story-page">
             <h1>You Made it to Grandma's House</h1>
@@ -16,6 +21,24 @@ function GranRightPath() {
             <button onClick={()=>handleClick("/rightpath1")}>Win</button>
             <button onClick={()=>handleClick("/rightpath2")}>Lose</button>
 
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <legend>Choose Your Answer</legend>
+                    <div>
+                        <input type="checkbox" id="riddle-answer-1" name="toolbox" value="wrong"></input>
+                        <label for="riddle-answer-1">Wrong Answer 1</label>
+                        <input type="checkbox" id="riddle-answer-2" name="toolbox" value="wrong"></input>
+                        <label for="riddle-answer-1">Wrong Answer 2</label>
+                        <input type="checkbox" id="riddle-answer-3" name="toolbox" value="wrong"></input>
+                        <label for="riddle-answer-1">Wrong Answer 3</label>
+                        <input type="checkbox" id="riddle-answer-1" name="toolbox" value="correct"></input>
+                        <label for="riddle-answer-1">Correct Answer</label>
+                    </div>
+                    <div>
+                        <button type="submit">Submit Answer</button>
+                    </div>
+                </fieldset>
+            </form>
         </div>
     )
 }
